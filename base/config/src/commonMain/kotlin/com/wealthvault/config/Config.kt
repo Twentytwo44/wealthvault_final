48691376bedc51db29bfa3e28b4a48dfbc0901d5
Natchapon Ponlaem <natchapon.ponl@kmutt.ac.th>
2026-05-10 00:38:35 +0700

    commit apk release

--- base/config/src/commonMain/kotlin/com/wealthvault/config/Config.kt
+++ base/config/src/commonMain/kotlin/com/wealthvault/config/Config.kt
@@ -1,10 +1,10 @@
 package com.wealthvault.config
 object Config {
-    const val localhost_android = "http://10.0.2.2:8080/api/"
+    const val localhost_android = "http://localhost:8080/api/"
 //    const val localhost_android = "http://localhost:8080/api/"
     const val localhost_ios = "http://localhost:8080/api/"
 
-    const val ws_android = "ws://10.0.2.2:8080/api/"
+    const val ws_android = "ws://localhost:8080/api/"
     const val ws_ios = "ws://localhost:8080/api/"
 
     val webSocketUrl: String
